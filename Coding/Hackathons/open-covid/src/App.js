ab5dae3b5741338559307fc3809af8398ecd0205
Your Name <k.soto@pucp.pe>
2021-03-06 15:09:44 -0500

    first push

---
+++ Coding/Hackathons/open-covid/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from './Home'
+
+
+function App() {
+  return (
+
+    // BEM
+    <div className="app">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ Coding/Hackathons/open-covid/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='home'>
+           <h1>Selecciona tu distrito</h1> 
+           {/* Box - busqueda de distrito */}
+        </div>
+    )
+}
+
+export default Home
